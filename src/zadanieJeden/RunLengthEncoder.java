96a94e1041b8f44e2f634a4adaca2c5c69e8c03f
Kamil Ochnik <ilmak@o2.pl>
2019-11-27 17:37:27 +0100

    implemented encoder method

--- src/zadanieJeden/RunLengthEncoder.java
+++ src/zadanieJeden/RunLengthEncoder.java
@@ -1,12 +1,27 @@
 package zadanieJeden;
 
- class RunLengthEncoder {
+class RunLengthEncoder {
 
-    String encoder(String plainText){
-        return "A4,B3,C1,112"+ plainText;
+    String encoder(String plainText) {
+        char[] input = plainText.toCharArray();
+
+        String output = Character.toString(input[0]);
+        int numberSameChars = 1;
+
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] == input[(i - 1)]) {
+                numberSameChars++;
+            } else {
+                output = output + numberSameChars+",";
+                numberSameChars=1;
+                char x = input[i];
+                output = output + x;
+            }
+        }  output = output + numberSameChars;
+        return output;
     }
 
-    String decode(String encodedText){
-        return "AAAABBBC111111111111"+ encodedText;
+    String decode(String encodedText) {
+        return "AAAABBB111111111111" + encodedText;
     }
 }
